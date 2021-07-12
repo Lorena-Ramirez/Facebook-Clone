44a3bbd36dc1a3afac403de8ecfccb35b6a525b7
Lorena Ramirez <ramirezlorenaa18@gmail.com>
2021-07-12 15:18:02 -0400

    imported useStateValue user and dynamically sent a src to Avatar component as well as dynamically sent username for header h4 display

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -13,8 +13,11 @@ import AddIcon from '@material-ui/icons/Add';
 import ForumIcon from '@material-ui/icons/Forum';
 import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { useStateValue } from '../../StateProvider';
 
 function Header() {
+  const [{ user }, dispatch] = useStateValue();
+
   return (
     <div className="header">
       <div className="header___left">
@@ -48,8 +51,8 @@ function Header() {
 
       <div className="header___right">
         <div className="header___info">
-          <Avatar />
-          <h4>Lorenaaaa</h4>
+          <Avatar src={user.photoURL} />
+          <h4>{user.displayName}</h4>
         </div>
         <IconButton>
           <AddIcon />

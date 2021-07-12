30715f5e673f6944876d4e11ae5152a697f6470b
Lorena Ramirez <ramirezlorenaa18@gmail.com>
2021-07-12 14:07:07 -0400

    imported firebase and created database variables, auth and provider variables

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCf3zV1u3Puwi8lW4Re7LsJh4xBUvl5Bwg",
   authDomain: "facebook-clone-18d77.firebaseapp.com",
@@ -8,3 +10,11 @@ const firebaseConfig = {
   appId: "1:1031970594601:web:74a416e5dc26f202b2e138",
   measurementId: "G-BEC8BRJ2HW"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;

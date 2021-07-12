2d683f108fe2390d482d3a41c830134322660aa5
Lorena Ramirez <ramirezlorenaa18@gmail.com>
2021-07-12 18:18:41 -0400

    changed timestamp to be line to display actual time instead of placeholder

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -19,8 +19,7 @@ function Post({ profilePic, image, username, timestamp, message }) {
         />
         <div className="post___topInfo">
           <h3>{username}</h3>
-          <p>Timestamp..</p>
-          {/* <p>{new Date(timestamp?.toDate()).toUTCString()}</p> */}
+          <p>{new Date(timestamp?.toDate()).toUTCString()}</p>
         </div>
       </div>
 
